799a3a872650f2d58134566176ff0d9f0d1d13a5
whllhw <990230292@qq.com>
2017-09-13 19:21:16 +0800

    fix

--- deploy.py
+++ deploy.py
@@ -5,8 +5,7 @@ import time
 def deploy(data):
     filename = data.split('title: ',1)[1].split('\n',1)[0]
     with open('_posts/'+filename+'.md','wb') as f:
-        print(type(data))
-        f.writelines(data)
+        f.writelines(data.encode('utf-8'))
     main(filename)
     return filename
     
