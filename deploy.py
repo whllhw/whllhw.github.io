724bb2f4873c0e226b28437feb3445116175d2d7
whllhw <990230292@qq.com>
2017-09-13 19:48:12 +0800

    fix

--- deploy.py
+++ deploy.py
@@ -7,7 +7,7 @@ def deploy(data):
     filename = data.split('title: ',1)[1].split('\n',1)[0].replace('-','').replace('/','').replace('\\','')
     with open('_posts/'+filename+'.md','wb') as f:
         f.writelines(data.encode('utf-8'))
-    Process(target=main,args=(data,)).start()
+    Process(target=main,args=(filename,)).start()
     return filename
     
 def main(filename):
