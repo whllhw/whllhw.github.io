b203abe1ebb104adf26333e6fdaaa19dd86f1c48
whllhw <990230292@qq.com>
2017-09-17 10:02:58 +0800

    test

--- deploy.py
+++ deploy.py
@@ -4,6 +4,7 @@ import os
 import time
 from multiprocessing import Process
 def deploy(data):
+    os.makedirs('_post') if not os.path.exists('_posts')
     filename = data.split('title: ',1)[1].split('\n',1)[0].replace('-','').replace('/','').replace('\\','')
     with open('_posts/'+filename+'.md','wb') as f:
         f.writelines(data.encode('utf-8'))
