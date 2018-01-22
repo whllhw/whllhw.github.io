95c0510f24b9b04793067d796be79c4511ff5410
whllhw <990230292@qq.com>
2018-01-22 15:50:06 +0800

    增加文件表，记录文件对应关系

--- models.py
+++ models.py
@@ -1,9 +1,10 @@
-from sqlalchemy import Column, Integer, String,ForeignKey,Text,DateTime
+from sqlalchemy import Column, Integer, String, ForeignKey, Text, DateTime
 from database import Base
 import datetime
 
+
 class User(Base):
-    __tablename__ ='User'
+    __tablename__ = 'User'
     id = Column(Integer, primary_key=True)
     username = Column(String(10), unique=True)
     password = Column(String(10))
@@ -18,15 +19,16 @@ class User(Base):
     def __str__(self):
         return '<User %r>' % self.username
 
+
 class Passage(Base):
     __tablename__ = 'Passage'
     id = Column(Integer, primary_key=True)
     title = Column(String(50))
-    author_id = Column(Integer,ForeignKey('User.id'))
+    author_id = Column(Integer, ForeignKey('User.id'))
     context = Column(Text)
     date = Column(DateTime)
 
-    def __init__(self,title,context,author,date=None):
+    def __init__(self, title, context, author, date=None):
         self.title = title
         self.context = context
         self.author_id = author
@@ -38,12 +40,13 @@ class Passage(Base):
     def __str__(self):
         return self.__repr__()
 
-class Category(Base):#存放标签名
-    __tablename__ = 'Category'
-    id = Column(Integer,primary_key = True)
-    name = Column(String(10),unique=True)
 
-    def __init__(self,name):
+class Category(Base):  # 存放标签名
+    __tablename__ = 'Category'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(10), unique=True)
+
+    def __init__(self, name):
         self.name = name
 
     def __repr__(self):
@@ -52,15 +55,38 @@ class Category(Base):#存放标签名
     def __str__(self):
         return '<Category %r>' % self.name
 
-class Tag(Base):#存放一对多的标签对应关系
-    __tablename__ ='Tag'
-    id = Column(Integer,primary_key=True)
-    passage_id = Column(Integer,ForeignKey('Passage.id'))
-    category_id = Column(Integer,ForeignKey('Category.id'))
-    def __init__(self,passage_id,category_id):
+
+class Tag(Base):  # 存放一对多的标签对应关系
+    __tablename__ = 'Tag'
+    id = Column(Integer, primary_key=True)
+    passage_id = Column(Integer, ForeignKey('Passage.id'))
+    category_id = Column(Integer, ForeignKey('Category.id'))
+
+    def __init__(self, passage_id, category_id):
         self.passage_id = passage_id
         self.category_id = category_id
+
     def __repr__(self):
         return '<Tag %r>' % self.id
+
     def __str__(self):
         return '<Tag %r>' % self.id
+
+
+class File(Base):  # 记录文件md5以及文件名
+    __tablename__ = 'File'
+    id = Column(Integer, primary_key=True)
+    passage_id = Column(Integer, ForeignKey('Passage.id'))
+    md5 = Column(String(32))
+    fileName = Column(String(50))
+
+    def __init__(self, passage_id, md5, fileName):
+        self.md5 = md5
+        self.passage_id = passage_id
+        self.fileName = fileName
+
+    def __repr__(self):
+        return '<File %r>' % self.id
+
+    def __str__(self):
+        return self.__repr__()
